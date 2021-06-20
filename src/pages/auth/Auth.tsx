a40556cda7b49f3b3cbd873b10ed8ba0e6c3605c
mxzpytlk <mxzpytlk@gmail.com>
2021-06-20 21:37:54 +0300

    Add auth page background

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,9 +1,14 @@
 import { Navbar } from "../../components/navbar/Navbar";
+import animatedLogo from '../../assets/back-animation.svg';
+import './auth.scss';
 
 export function Auth() {
   return (
-    <div>
+    <div className='auth'>
       <Navbar />
+      <div className='auth__container'>
+        <img className='auth__animated' src={animatedLogo} alt="Animated logo"/>
+      </div>
     </div>
   );
 }
